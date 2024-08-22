6391ba86a25e3693e59d76c9cbcd4b4c6fc7e985
Gaston <gastonmolina267@gmail.com>
2024-08-22 16:20:43 -0300

    Tarea07

--- Tarea06/Calculadora_Tarea06/obj/Debug/net8.0-windows/Calculadora_Tarea06.AssemblyInfo.cs
+++ Tarea06/Calculadora_Tarea06/obj/Debug/net8.0-windows/Calculadora_Tarea06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3cedee3bac9f6df59e0c5cb74197688c76b7ece")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6631f9c9c1c8fda95f399f4420e32eb8984b589")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
