dcf6d3bc3c0074f16fbb2f7b8dcc644d47fee904
Gaston <gastonmolina267@gmail.com>
2024-09-03 12:48:01 -0300

    Cambiando_README

--- Tarea06/Calculadora_Tarea06/obj/Debug/net8.0-windows/Calculadora_Tarea06.AssemblyInfo.cs
+++ Tarea06/Calculadora_Tarea06/obj/Debug/net8.0-windows/Calculadora_Tarea06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cf971b1744f6310b2bbb73e9dd7d28829ffff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce645bcffe8131174912674c246562e38e4e5751")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora_Tarea06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
